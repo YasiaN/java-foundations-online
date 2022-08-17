fc3d90d1d237dd3374630a5372d8f433efc1d00a
YasiaN <yaroslavna.nezhinskaya@coins-global.com>
2022-08-17 16:34:23 +0400

    Added classwork and homework

---
+++ src/main/java/ru/itsjava/collections/lists/maps/Fruit.java
@@ -0,0 +1,15 @@
+package ru.itsjava.collections.lists.maps;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Fruit {
+    private String name;
+    private double weight;
+
+
+
+
+}
---
+++ src/main/java/ru/itsjava/collections/lists/maps/Vegetables.java
@@ -0,0 +1,12 @@
+package ru.itsjava.collections.lists.maps;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Vegetables {
+    private String name;
+    private double weight;
+
+}
