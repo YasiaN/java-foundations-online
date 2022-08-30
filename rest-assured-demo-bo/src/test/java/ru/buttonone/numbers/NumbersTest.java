0d0f4b28dcc58cdecae06609cc224f74e88a185f
YasiaN <yaroslavna.nezhinskaya@coins-global.com>
2022-08-30 19:22:13 +0400

    Homework

--- rest-assured-demo-bo/src/test/java/ru/buttonone/numbers/NumbersTest.java
+++ rest-assured-demo-bo/src/test/java/ru/buttonone/numbers/NumbersTest.java
@@ -4,11 +4,12 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class NumbersTest {
 
-    public static final String NUMBERS_URL = Numbers_URL;
+    public static final String NUMBERS_URL = "Numbers_URL";
 
     @Test
     public void shouldHaveCorrectGet2() {
@@ -28,6 +29,44 @@ public class NumbersTest {
                 .contentType(ContentType.TEXT)
                 .log().all()
                 .statusCode(200);
+    }
+
+    @DisplayName("Проверяем, что при вводе числа выдаёт ответ и ответ релевантен ")
+    @Test
+    public void shouldHaveCorrectGet8 () {
+
+       //given
+//        Response response = RestAssured.given().get("http://numbersapi.com/8/math");
+//        response.prettyPrint();
+
+        RestAssured
+                .given()
+                .baseUri("http://numbersapi.com/8")
+                .when()
+                .get("/math")
+                .then()
+                .log().all()
+                .statusCode(200);
+            }
+
+    @DisplayName("Проверяем, что при вводе даты выдаёт ответ и ответ релевантен")
+    @Test
+    public void shouldHaveCorrectDate27Aug () {
+
+        RestAssured
+                .given()
+                .baseUri("http://numbersapi.com/")
+                .when()
+                .get("8/27/date")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .contentType(ContentType.TEXT);
+
 
     }
+
+
+
+
 }
